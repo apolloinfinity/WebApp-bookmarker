9c4eef36a1f77fdadc56709d79d281ea9ddb83a2
Javier Ramirez <apollo.infinity88@gmail.com>
2018-01-14 22:24:01 -0800

    finished first function that stores data into the localstorage

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 document.getElementById('myForm').addEventListener('submit', saveBookmark);
-
 function saveBookmark(e){
     // Get Values
     var siteName =  document.getElementById('siteName').value;
@@ -18,14 +17,44 @@ function saveBookmark(e){
     console.log(localStorage.getItem('test'));
     */
 
-    if(localStorage.getItem('bookmarks')=== null) {
+    if(localStorage.getItem('bookmarks') === null) {
         //Init Array
         var bookmarks = [];
         bookmarks.push(bookmark);
         // Set to localStorage
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    } else {
+        // Get bookmarks from LocalStorage
+        var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+        // Add book to array
+        bookmarks.push(bookmark);
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+
     }
 
     // Prevents default behavior of form from submitting
     e.preventDefault();
+}
+
+// Fetch Bookmarks
+function fetchBookmarks(){
+
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+    // Get ouput id
+    var bookmarksResults = document.getElementById('bookmarksResults');
+
+    // Build output
+    bookmarksResults.innerHTML = '';
+    for(var i = 0; i < bookmarks.length; i++) {
+        var name = bookmarks[i].name;
+        var ulr = bookmarks[i].url;
+
+        bookmarksResults.innerHTML += 
+        '<div class="well">' + 
+        '<h3>' + name + '</h3>' +
+        '</div>'
+
+    }
 }
