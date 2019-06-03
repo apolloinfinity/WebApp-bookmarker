9545d63efbd9309c3e1ae95e53f8580325b22aa4
Javier Ramirez <apollo.infinity88@gmail.com>
2019-06-03 09:29:42 -0700

    Added mongoose to node backend, set up connection without erros

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const dotenv = require('dotenv')
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const db = require('./config/db').mongoURI;
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(err => console.error(err));
+
+
 
 app.get('/', (req, res, next) => {
     console.log('Hello');
     res.send('Hello')
     res.status(200);
     next();
-})
+});
 
 app.post('/bookmarks', (req, res) => {
     console.log("Saved!")
     res.send('Saved!');
     res.status(201);
-})
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port: ${port}`));
