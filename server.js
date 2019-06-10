099091ca1b9ef90a8297973cdc3ed606f36f3107
Javier Ramirez <apollo.infinity88@gmail.com>
2019-06-09 19:42:24 -0700

    Changed path for client directory and routes to their own paths

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -12,7 +14,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', require('./routes/bookmark'));
+app.use('/', express.static(path.join(__dirname, 'client')));
+app.use('/api', require('./routes/bookmark'));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port: ${port}`));
