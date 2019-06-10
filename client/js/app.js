93b2d34fe424368de2a488742cc592ee9e972704
Javier Ramirez <apollo.infinity88@gmail.com>
2019-06-10 12:06:32 -0700

    changed fetchBookmarks() to use fetch API to get data from Backend/DB

--- client/js/app.js
+++ client/js/app.js
@@ -14,11 +14,31 @@ function validateForm(siteName, siteUrl) {
     var regex = new RegExp(expression);
 
     if (!siteUrl.match(regex)) {
-        alert("Please use a valid URL");
+        alert('Please use a valid URL');
         return false;
     }
 
     return true;
 }
 
-fetch('')
+async function fetchBookmarks() {
+
+    const bookmarksResults = document.getElementById('bookmarksResults');
+
+    let response = await (await fetch('http://127.0.0.1:5000/api/bookmarks'));
+    let bookmarks = await response.json();
+
+    for (bookmark of bookmarks) {
+        let name = bookmark.bookmarkName;
+        let url = bookmark.bookmarkURL;
+        bookmarksResults.innerHTML +=
+            `<div class="well">
+            <h3>${name}
+                <a class="btn btn-default" target="_blank" href="${url}">Visit</a>
+                <a onclick="deleteBookmark('${url}')" class="btn btn-danger" href="">Delete</a>
+            </h3>
+        </div>
+        `
+    };
+
+}
