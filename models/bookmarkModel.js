1151ddacff30a834a4ca32695eb9840c8919337b
Javier Ramirez <apollo.infinity88@gmail.com>
2019-06-17 15:43:02 -0700

    Changed schema settings for easier to read requests

--- models/bookmarkModel.js
+++ models/bookmarkModel.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const BookmarkSchema = new Schema({
-    bookmarkName: String,
-    bookmarkURL: String,
+    name: String,
+    url: String,
     date: { type: Date, default: Date.now }
 });
 
