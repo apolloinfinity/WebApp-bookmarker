10c79364dfef30f09646495a5f21363b879c5188
Javier Ramirez <apollo.infinity88@gmail.com>
2019-06-18 15:19:38 -0700

    Formatted models for better uniform syntax

--- models/bookmarkModel.js
+++ models/bookmarkModel.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const BookmarkSchema = new Schema({
-    name: String,
-    url: String,
-    date: { type: Date, default: Date.now }
+	name: String,
+	url: String,
+	date: { type: Date, default: Date.now }
 });
 
 const Bookmark = mongoose.model('bookmarks', BookmarkSchema);
