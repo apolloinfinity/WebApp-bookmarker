50caa1c03713cbc1752603a4ba33b8d623022f27
Javier Ramirez <apollo.infinity88@gmail.com>
2019-12-11 20:04:50 -0800

    Changed route to be simple
    '/'

--- server.js
+++ server.js
@@ -7,18 +7,24 @@ const cors = require('cors');
 
 const db = require('./config/db').mongoURI;
 mongoose
-  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error(err));
+	.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => console.log('Connected to MongoDB'))
+	.catch((err) => console.error(err));
 
 const app = express();
+
+const bookmarks = require('./routes/bookmark');
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', express.static(path.join(__dirname, 'client')));
-app.use('/api', require('./routes/bookmark'));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', express.static(path.join(__dirname, 'cl')));
+app.use('/', bookmarks);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port: ${port}`));
