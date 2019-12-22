41e6aab3fea602aefbcf7da454e2cf0014a76270
Javier Ramirez <apollo.infinity88@gmail.com>
2019-12-21 22:21:07 -0800

    Updated packages and removed body parser, using asyn/await for db and staring up app

--- server.js
+++ server.js
@@ -1,30 +1,33 @@
 const path = require('path');
 
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
 const db = require('./config/db').mongoURI;
-mongoose
-	.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-	.then(() => console.log('Connected to MongoDB'))
-	.catch((err) => console.error(err));
-
-const app = express();
-
 const bookmarks = require('./routes/bookmark');
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+const start = async () => {
+	try {
+		await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
+		console.log('Connected to MongoDB');
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-app.use(express.static(path.join(__dirname, 'public')));
+		const app = express();
 
-// app.use('/', express.static(path.join(__dirname, 'cl')));
-app.use('/', bookmarks);
+		app.use(cors());
+		app.use(express.json());
+		app.use(express.urlencoded({ extended: false }));
+		app.set('view engine', 'ejs');
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Listening on port: ${port}`));
+		app.set('views', 'views');
+		app.use(express.static(path.join(__dirname, 'public')));
+		app.use('/', bookmarks);
+
+		const port = process.env.PORT || 5000;
+		app.listen(port, () => console.log(`Listening on port: ${port}`));
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+start();
