e69b4da9cb4d6c2bf3b43f7ba5b1f8305cd95c42
Javier Ramirez <apollo.infinity88@gmail.com>
2019-12-26 11:31:09 -0800

    removed and and renamed some routes for less confusion and in keeping with API standars

--- routes/bookmark.js
+++ routes/bookmark.js
@@ -1,17 +1,11 @@
 const { Router } = require('express');
 const router = Router();
 
-const {
-	getBookmarks,
-	getBookmark,
-	postBookmark,
-	updateBookmark,
-	deleteBookmark
-} = require('../controllers/bookmarkController');
+const { getBookmarks, postBookmark, deleteBookmark } = require('../controllers/bookmarkController');
 
 router.get('/', getBookmarks);
-router.get('/bookmarks/:id', getBookmark);
+// router.get('/bookmarks/:id', getBookmark);
 router.post('/bookmark', postBookmark);
-router.delete('/bookmarks/:id', deleteBookmark);
+router.delete('/bookmark/:id', deleteBookmark);
 
 module.exports = router;
