eb18b1b49deaee63ccae4eca6786b0ab12343b4f
Javier Ramirez <apollo.infinity88@gmail.com>
2019-12-26 11:24:41 -0800

    Changed order of urlencode and json

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ const start = async () => {
 		const app = express();
 
 		app.use(cors());
-		app.use(express.json());
 		app.use(express.urlencoded({ extended: false }));
+		app.use(express.json());
 		app.set('view engine', 'ejs');
 
 		app.set('views', 'views');
