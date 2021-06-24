7e4cbe2bdcf590509bdccae04c9b17ee94fac2c5
Javier Ramirez Roa <apollo.infinity88@gmail.com>
2021-06-24 10:45:16 -0700

    Moved port variable to top for consistency, removed any code regarding ejs

--- server.js
+++ server.js
@@ -6,43 +6,40 @@ const cors = require('cors');
 
 require('dotenv').config();
 const bookmarks = require('./routes/bookmark.routes');
+const port = process.env.PORT || 5000;
 
 db = process.env.MONGO_URI;
 
 // const demoLogger = require('./helpers/logger');
 
 const start = async () => {
-  try {
-    await mongoose.connect(db, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      dbName: 'bookmarks',
-    });
-    console.log('Connected to MongoDB');
+	try {
+		await mongoose.connect(db, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			dbName: 'bookmarks'
+		});
+		console.log('Connected to MongoDB');
 
-    const app = express();
+		const app = express();
 
-    app.use(cors());
-    app.use(express.urlencoded({ extended: true }));
-    app.use(express.json());
-    app.set('view engine', 'ejs');
+		app.use(cors());
+		app.use(express.urlencoded({ extended: true }));
+		app.use(express.json());
 
-    // app.use(demoLogger);
+		// app.use(demoLogger);
 
-    app.set('views', 'views');
-    app.use(express.static(path.join(__dirname, 'public')));
-    app.use('/api', bookmarks);
+		app.use('/api', bookmarks);
 
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'public/'));
-    });
+		app.get('*', (req, res) => {
+			res.sendFile(path.join(__dirname, 'public/'));
+		});
 
-    const port = process.env.PORT || 5000;
-    app.listen(port, () => console.log(`Listening on port: ${port}`));
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
+		app.listen(port, () => console.log(`Listening on port: ${port}`));
+	} catch (err) {
+		console.error(err);
+		process.exit(1);
+	}
 };
 
 start();
