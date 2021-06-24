4666f72b2bf25b572f084fd1d3cc5974f0bada75
Javier Ramirez Roa <apollo.infinity88@gmail.com>
2021-06-24 10:45:49 -0700

    Removed description in favor with urlName like the original App.

--- models/bookmark.model.js
+++ models/bookmark.model.js
@@ -1,10 +1,10 @@
 const { Schema, model } = require('mongoose');
 
 const BookmarkSchema = new Schema({
-  url: String,
-  description: String,
-  user: String,
-  created: { type: Date, default: Date.now },
+	url: String,
+	urlName: String,
+	user: String,
+	created: { type: Date, default: Date.now }
 });
 
 module.exports = Bookmark = model('bookmark', BookmarkSchema);
