e82728563f64c49962c83373406bb2c9f6601ee6
Javier Ramirez Roa <apollo.infinity88@gmail.com>
2021-06-24 10:44:10 -0700

    Changed the req.body properties from description to urlName

--- controllers/bookmark.controller.js
+++ controllers/bookmark.controller.js
@@ -1,46 +1,45 @@
 const Bookmark = require('../models/bookmark.model');
 
 exports.getBookmarks = async (req, res) => {
-  try {
-    const bookmarks = await Bookmark.find({});
+	try {
+		const bookmarks = await Bookmark.find({});
 
-    return res.status(200).json({ data: bookmarks });
-  } catch (err) {
-    return res.status(400).json({ error: err });
-  }
+		return res.status(200).json({ data: bookmarks });
+	} catch (err) {
+		return res.status(400).json({ error: err });
+	}
 };
 
 exports.postBookmark = async (req, res) => {
-  try {
-    const { description, url, user } = await req.body;
-    // console.log(req.body);
-    const bookmark = Bookmark({
-      description,
-      url,
-      user,
-    });
+	try {
+		const { url, user, urlName } = await req.body;
+		const bookmark = Bookmark({
+			url,
+			urlName,
+			user
+		});
 
-    const checkURL = await Bookmark.findOne({ url: url });
-    if (checkURL) {
-      return res.json('Bookmark already exists!');
-    } else {
-      await bookmark.save();
-      console.log('Saved');
-      return res.status(201).json({ data: bookmark });
-    }
-  } catch (err) {
-    return res.status(400).json({ error: err });
-  }
+		const checkURL = await Bookmark.findOne({ url: url });
+		if (checkURL) {
+			return res.json('Bookmark already exists!');
+		} else {
+			await bookmark.save();
+			console.log('Saved');
+			return res.status(201).json({ data: bookmark });
+		}
+	} catch (err) {
+		return res.status(400).json({ error: err });
+	}
 };
 
 exports.deleteBookmark = async (req, res) => {
-  try {
-    const id = await req.params.id;
+	try {
+		const id = await req.params.id;
 
-    await Bookmark.deleteOne({ _id: id });
+		await Bookmark.deleteOne({ _id: id });
 
-    return res.status(200).json({ data: 'Bookmark deleted' });
-  } catch (err) {
-    return res.status(400).json({ error: err });
-  }
+		return res.status(200).json({ data: 'Bookmark deleted' });
+	} catch (err) {
+		return res.status(400).json({ error: err });
+	}
 };
