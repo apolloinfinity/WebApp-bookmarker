43e466b5e5b60b02e89483af2ebccbd535a96221
Javier Ramirez Roa <javier.roa@beckshybrids.com>
2022-08-21 20:34:23 -0700

    Updating packages

--- server.js
+++ server.js
@@ -6,40 +6,37 @@ const cors = require('cors');
 
 require('dotenv').config();
 const bookmarks = require('./routes/bookmark.routes');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5050;
 
 db = process.env.MONGO_URI;
 
 // const demoLogger = require('./helpers/logger');
 
 const start = async () => {
-	try {
-		await mongoose.connect(db, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			dbName: 'bookmarks'
-		});
-		console.log('Connected to MongoDB');
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: 'bookmarks',
+    });
+    console.log('Connected to MongoDB');
 
-		const app = express();
+    const app = express();
 
-		app.use(cors());
-		app.use(express.urlencoded({ extended: true }));
-		app.use(express.json());
+    app.use(cors());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
 
-		// app.use(demoLogger);
-		app.use(express.static('public'));
+    // app.use(demoLogger);
+    app.use(express.static('public'));
 
-		app.get('/', (req, res) => {
-			res.sendFile(path.join(__dirname, 'public/'));
-		});
-		app.use('/api', bookmarks);
+    app.use('/api', bookmarks);
 
-		app.listen(port, () => console.log(`Listening on port: ${port}`));
-	} catch (err) {
-		console.error(err);
-		process.exit(1);
-	}
+    app.listen(port, () => console.log(`Listening on port: ${port}`));
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 };
 
 start();
